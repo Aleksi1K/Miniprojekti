74cf7cce4e7c0a728db870eda0ee94eb2f6eab18
Aleksi1K <59920287+Aleksi1K@users.noreply.github.com>
2020-03-04 15:29:02 +0200

    yhteisprojekti

---
+++ todo-projekti/public/javascripts/script.js
@@ -0,0 +1,186 @@
+const taskData = document.getElementById("task");
+const button = document.getElementById("button");
+const taskList = document.getElementById("task-list");
+const filter = document.getElementById("filter")
+const editInput = document.getElementById('task-edit')
+
+// Tallennetaan taskit myös tähän arrayhin filtteröinnin helpottamiseksi
+let currentTasks = [];
+
+button.addEventListener("click", postTask);
+window.addEventListener("DOMContentLoaded", getTasks);
+taskList.addEventListener("click", updateOrDeleteTask);
+filter.addEventListener("keyup", filterTasks);
+
+
+function postTask(e) {
+  //Tarkistetaan inputin pituus
+  if (taskData.value === "") {
+    validateInput("empty")
+    return;
+  }
+  if (taskData.value.length>40){
+    validateInput("long")
+    return;
+  }
+  //Lähetetään form data Fetch POSTilla:
+  fetch("/api/tasks", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      task: taskData.value
+    })
+  })
+    .then(res => {
+      if (res.status === 201) {
+        console.log("Post request successful!");
+      }
+      return res.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+  location.reload();
+  // Varmuuden vuoksi estetään tavallisesti tapahtuva:
+  e.preventDefault();
+}
+
+async function getTasks() {
+  try {
+    const response = await fetch("/api/tasks");
+    const fetchedTasks = await response.json();
+    //Tallennetaan taskit myös arrayhyn:
+    currentTasks = fetchedTasks;
+    addTasksToSite(fetchedTasks);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function updateOrDeleteTask(e) {
+  // Haetaan taskin arvot event-parametrilta
+  let taskName = e.target.parentElement.parentElement.firstChild.innerText;
+  let taskId = e.target.parentElement.parentElement.firstChild.id;
+
+  // Kutsutaan funktiota updateTask tai deleteTask riippuen siitä, kumpaa nappia ollaan painettu
+
+  if (e.target.innerHTML === "Edit") {
+    updateTask(taskName, taskId);
+  }
+  if (e.target.innerHTML === "Delete") {
+    deleteTask(taskId);
+  }
+}
+
+function addTasksToSite(tasks) {
+  for (let t of tasks) {
+    let li = document.createElement("li");
+    li.innerHTML = `<p id="${t.id}">${t.task}</p> <span><button id="update-task">Edit</button><button id="delete-task">Delete</button></span>`;
+    taskList.appendChild(li);
+  }
+}
+
+function deleteTask(id) {
+  fetch(`/api/tasks/${id}`, {
+    method: "DELETE"
+  })
+    .then(res => {
+      if (res.status === 200) {
+        console.log("Delete request successful!");
+      }
+      return res.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+  location.reload();
+}
+
+
+function updateTask(task, id) {
+  const editTask = document.getElementById('edit-task');
+  const addTask = document.getElementById('add-task');
+  const allTasks = document.getElementById('tasks')
+
+  // Muut sivun osuudet piiloon: 
+  editTask.style.display = "block"
+  addTask.style.display = "none"
+  allTasks.style.display = "none"
+
+  //Asetetaan task valmiiksi input-kenttään:
+  editInput.value = task;
+
+  // Jos cancelia painettu, muutetaan displayt takaisin alkuperäisiksi
+  document.getElementById('button-cancel').addEventListener("click", (e)=> {
+    location.reload();
+  })
+  // Submitilla ktusutaan funktiota, joka lähettää PUT-requestin palvelimelle
+  document.getElementById('button-edit').addEventListener("click", (e)=> {
+    if (editInput.value === "") {
+      alert("Please add a task description!");
+      return;
+    }else if (editInput.value.length > 45){
+      alert("Max length for task is 45 characters. Please readjust your task desciption.")
+      return;
+    }
+    sendUpdatedTaskToServer(id, editInput.value)
+  })
+}
+
+function sendUpdatedTaskToServer(id, updatedTask){
+  fetch(`/api/tasks/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      task: updatedTask
+    })
+  })
+    .then(res => {
+      if (res.status === 200 || res.status === 201) {
+        console.log("PUT request successful!");
+        getTasks();
+      }
+      return res.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+    location.reload();
+}
+
+
+async function filterTasks(e) {
+  console.log(e.target.value);
+  let searchParam = e.target.value.trim().toLowerCase();
+  for (let t of currentTasks) {
+    if (t.task.toLowerCase().startsWith(searchParam)) {
+      document.getElementById(t.id).parentElement.style.display = "flex";
+    } else {
+      document.getElementById(t.id).parentElement.style.display = "none";
+    }
+  }
+}
+
+function validateInput(param){
+  if (param === "empty"){
+    let errorMessage = "Task cannot be empty!"
+    let taskInput = document.querySelector(".task-input")
+    document.querySelector('small').innerText = errorMessage;
+    taskInput.classList.toggle("error")
+    setTimeout(()=>{
+      taskInput.classList.toggle("error")
+    },3000)
+  } else if (param === "long") {
+    let errorMessage = "Task cannot be longer than 40 characters!"
+    let taskInput = document.querySelector(".task-input")
+    document.querySelector('small').innerText = errorMessage;
+    taskInput.classList.toggle("error")
+    setTimeout(()=>{
+      taskInput.classList.toggle("error")
+    },3000)
+  }
+}
---
+++ todo-projekti/routes/tasks.js
@@ -0,0 +1,85 @@
+// Titta ja Aleksi
+
+var express = require('express');
+var router = express.Router();
+var fs = require('fs'); //voidaan kirjoittaa tiedostoon, tasks.json
+var uuid = require('uuid/v4'); // automaattinen id:n luonti
+const cors = require ('cors'); // voidaan hakea tietoa ulkoisista apeista
+tasks = []; //Olemassa olevan tiedon lukeminen arrayhin tasks.json-tiedostosta tapahtuu app.js:stä
+
+//kirjoittaa tasks-arrayn sisällön tasks.json-tiedostoon.
+//Funktio kutsutaan, kun tietoa lisätään, poistetaan tai muokataan
+function write() {
+  fs.writeFile("tasks.json", JSON.stringify(tasks, null, 1),
+  function (err) {
+      if (err) throw err;
+    });
+  }
+
+//tiedon hakeminen ja lisääminen, ei id-kohtaista tietoa
+router.route('/')
+  .get(function (req, res, next) {
+    res.json(tasks);
+  })
+
+  .post(function (req, res, next) {
+    if (!req.body) throw new Error("Tyhjä syöte");
+    const t = req.body;
+    if (t.task) {
+      t.id = uuid();
+      let add = {id: t.id, task: t.task};
+      tasks.push(add);
+      write();
+      res.json(add);
+    } else {
+      throw new Error("Virheellinen syöte");
+    }
+
+  });
+
+  //tiedon hakeminen, muokkaaminen ja poistaminen id:n perusteella
+router.route('/:id')
+  .get(function (req, res) {
+    for (let task of tasks) {
+      if (task.id === req.params.id) {
+        res.json(task)
+        return
+      }
+    }
+    res.status(404)
+    res.json({ Viesti: "Taskia ei löydy!" })
+  })
+
+  .delete(function (req, res) {
+    let id = 0;
+    for (let task of tasks) {
+      if (task.id === req.params.id) {
+        tasks.splice(tasks.indexOf(task), 1)
+        write();
+        res.json({ Viesti: "Taski poistettu!" })
+        return
+      }
+    }
+    res.status(404)
+    res.json({ Viesti: "Virhe! Taskia ei löytynyt" })
+  })
+
+  .put(function (req, res) {
+    for (let t of tasks) {
+      if (t.id === req.params.id) {
+        const change = req.body;
+        if (change.task != '' && change.task != null) {
+          t.task = change.task;
+          write();
+          res.json({ Viesti: 'Muutettu' });
+          return
+        } else {
+          res.status(400).json( { Viesti: "Virhe! Task tyhjä" })
+        }
+      }
+    }
+    res.status(404)
+    res.json({ Viesti: "Virhe! Taskia ei löytynyt" })
+  });
+
+module.exports = router;
